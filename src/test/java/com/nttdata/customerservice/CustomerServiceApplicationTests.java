7228eee5ca4c4fc9b390a65dfba794d1577b4fc5
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-19 18:56:52 -0500

    Update for project 3

--- src/test/java/com/nttdata/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/com/nttdata/customerservice/CustomerServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nttdata.customerservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomerServiceApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
